68f05fb837935402abbc956f93a8d05f553ab14f
Joram Clervius <clervius@gmail.com>
2016-06-19 19:36:35 -0400

    think i fixed the problem with controller creation

--- public/app/account/mvNavBarLoginCtrl.js
+++ public/app/account/mvNavBarLoginCtrl.js
@@ -1,5 +1,5 @@
-angular.module=('app').controller('mvNavBarLoginCtrl', function($scope){
+angular.module('app').controller('mvNavBarLoginCtrl', function($scope){
 	$scope.signin = function(username, password){
 		console.log("I'm not done yet");
 	}
-})
+});
